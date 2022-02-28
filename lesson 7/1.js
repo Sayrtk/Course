add2e26b08a60a4a8ca865cc533a136cb7227492
Rita Karebo <ritakarebo@gmail.com>
2022-02-28 17:43:02 +0300

    changes

--- lesson 7/1.js
+++
@@ -1,9 +0,0 @@
-// 1. поменять массив в обратном порядке - [1,2,3,4,5,6] [6,5,4,3,2,1]
-
-let array = [1, 2, 3, 4, 5, 6,];
-
-function changeOrder() {
-    return array.reverse();
-}
-
-console.log(changeOrder(array)); 
--- lesson 7/6.js
+++
@@ -1,9 +0,0 @@
-// 6. удалить из массива все повторяющиеся элементы
-
-let arr = [1, 1, 2, 10, 2, 40, 50];
-
-let a1 = arr.filter((item, index) => {
-    return arr.indexOf(item) === index;
-})
-console.log(a1);
-
